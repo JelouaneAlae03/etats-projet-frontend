6b884deb44ebb208759dbc1a05b71bf50cdc504f
Mehdi JABRANE <jabranemehdi088@gmail.com>
2024-07-16 11:37:52 +0100

    modal insert

--- src/component/modal/MainModal.jsx
+++ src/component/modal/MainModal.jsx
@@ -2,22 +2,27 @@ import { Modal } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'; 
 import './index.css';
-import {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function MainModal({isShow,setIsShow,handleShow}){
-
+export default function MainModal(){
+    const dispatch = useDispatch();
+    const isShow = useSelector(state=>state.isShow);
+    const currentEtat = useSelector(state=>state.currentEtat);
+    const handleCloseModal = (value) => {
+        dispatch({type: 'MODAL_SHOW', payload: {value: value}});
+    }
     const data = [
         {id: 1, name: "mehdi", options: [{id: "M1", name: "XXX"}, {id: "M2", name: "YYY"}, {id: "M3", name: "ZZZ"}]},
         {id: 2, name: "walid", options: [{id: "W1", name: "XXX"}, {id: "W2", name: "YYY"}, {id: "W3",name: "ZZZ"}]},
     ]
     return(
         <>
-            <Modal dialogClassName="custom-modal-dialog" show={isShow} size='m' className='the-main-modal' animation={false} backdropClassName="custom-backdrop" centered>
+            <Modal onHide={() => handleCloseModal(false)} dialogClassName="custom-modal-dialog" show={isShow} size='m' className='the-main-modal' animation={true} backdropClassName="custom-backdrop" centered>
                 <div className='modal-content custom-modal-content'>
                     <Modal.Header className='header'>
-                        <Modal.Title className='main-header-title'>--Nom d'état--</Modal.Title>
+                        <Modal.Title className='main-header-title'>{currentEtat}</Modal.Title>
                         <div className='button-div'>
-                            <button className='header-right-button' onClick={() => handleShow(false)}><i className="bi bi-x-lg button-close"></i></button>
+                            <button className='header-right-button' onClick={() => handleCloseModal(false)}><i className="bi bi-x-lg button-close"></i></button>
                         </div>
                     </Modal.Header>
                     <Modal.Body className='body'>
@@ -25,13 +30,16 @@ export default function MainModal({isShow,setIsShow,handleShow}){
                             {data.map((d) => (
                                 <>
                                 <label htmlFor={`x-${d.name}`}>{d.name}</label>
-                                <select id={`x-${d.name}`}>
+                                <select id={`x-${d.name}`} className='form-select filtrage-select'>
                                     {d.options.map((dt)=>(
                                         <option value={dt.id}>{dt.name}</option>
                                     ))}
                                 </select>
                             </>
                             ))}
+                            <div className='btn-parent'>
+                                <button className='modal-submit-btn'>Aperçu</button>
+                            </div>
                         </div>
                     </Modal.Body>
                 </div>
